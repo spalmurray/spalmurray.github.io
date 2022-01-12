44bf121b4d34d0545fad5adfb0182625281e54c9
spalmurray <spalmurray@gmail.com>
2022-01-12 00:47:31 -0500

    removed resume

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
         <h1 className="my-name">Spencer Murray</h1>
         <p>Hi there! I haven't started working on the design of this page yet, but here are the important links:</p>
         <a href="https://github.com/spalmurray">My GitHub</a>
-        <a href="https://github.com/spalmurray/resume/blob/master/resume_spencermurray.pdf">My Resume</a>
         <h3>Things I've made:</h3>
         <a href="https://newt.social">Newt</a>
         <a href="https://www.spalmurray.com/wordle-helper">wordle-helper</a>
