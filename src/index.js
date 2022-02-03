910bde8baf9ec9961db044649c8c8ac7a1bffac1
spalmurray <spalmurray@gmail.com>
2022-02-02 20:15:24 -0500

    routing fix attempt 2: hash routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import App from './App';
 import Tos from "./routes/wordle-bot/tos";
 import Pp from "./routes/wordle-bot/pp";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/wordle-bot/terms-of-service" element={<Tos />} />
             <Route path="/wordle-bot/privacy-policy" element={<Pp />} />
         </Routes>
-    </BrowserRouter>,
+    </HashRouter>,
   document.getElementById('root')
 );
